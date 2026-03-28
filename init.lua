1cf7301550ee84448f0182cf77e2594dd32976e7
espocia <joshuajonellegarbin.espocia@gmail.com>
2026-03-28 21:46:13 +0800

    change: inline signs

--- init.lua
+++ init.lua
@@ -6,6 +6,12 @@ vim.cmd("set clipboard=unnamedplus")
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+local signs = { Error = "✘", Warn = "▲", Hint = "⚑", Info = "●" }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 vim.diagnostic.config({
 	virtual_text = {
 		prefix = "●",
